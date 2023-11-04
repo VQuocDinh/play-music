cee2127c281401b11db83f3ac2b641c0e6636213
VQuocDinh <vqdinh2202@gmail.com>
2023-11-04 12:01:22 +0700

    Xóa toàn bộ file và thư mục

--- src/routes/playlist.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const playlistController = require('../app/controllers/PlaylistController')
-
-router.get('/:playlistId', playlistController.getAllPlaylistSong)
-router.get('/', playlistController.getAllPlaylist)
-
-
-module.exports = router;
--- src/routes/song.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const songController = require('../app/controllers/SongController')
-
-router.get('/:songId', songController.getAllSong)
-
-
-
-module.exports = router;
